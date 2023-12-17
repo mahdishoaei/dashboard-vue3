50499a12f5c18c7d232a1adc45d3a471d3f9fe9e
alimpp <hmdmpp@gmail.com>
2023-12-17 06:23:48 -0800

    Make topbar for layout

--- src/layout/app/index.vue
+++ src/layout/app/index.vue
@@ -3,7 +3,39 @@
     <div>
       <sidebar />
     </div>
-    <div>
+    <div class="w-100" :class="{ 'app-bg-dark': theme == 'dark' }">
+      <div class="app-flex app-justify-end w-100 px-8 py-4">
+        <div
+          class="app-bg-info py-2 px-2 mx-2 app_border_radius app-font-size-14"
+        >
+          <i class="bi bi-list"></i>
+          Total Tasks 10
+        </div>
+        <div
+          class="app-bg-gray py-2 px-2 mx-2 app_border_radius app-font-size-14"
+        >
+          <i class="bi bi-person"></i>
+          Total Employe 118
+        </div>
+        <div
+          class="app-bg-secondary py-2 px-2 mx-2 app_border_radius app-font-size-14"
+        >
+          <i class="bi bi-shop"></i>
+          Total Products 3654
+        </div>
+        <div
+          class="app-bg-danger py-2 px-2 mx-2 app_border_radius app-font-size-14"
+        >
+          <i class="bi bi-cash-coin"></i>
+          Payment 100,235,000
+        </div>
+        <div
+          class="app-bg-warning py-2 px-2 mx-2 app_border_radius app-font-size-14"
+        >
+          <i class="bi bi-house-door"></i>
+          All Warehouses 8
+        </div>
+      </div>
       <RouterView />
     </div>
   </div>
@@ -15,13 +47,19 @@ import sidebar from "@/components/Sidebar/index.vue";
 import { useRouter } from "vue-router";
 import { usersDS } from "@/stores/usersDS";
 import { userDS } from "@/stores/userDS";
-import { onMounted } from "vue";
+import { applicationTheme } from "@/stores/applicationTheme";
+import { onMounted, computed } from "vue";
 import { getLoacalStorage } from "@/utils/localStorageServices";
 import { detectedUserByToken } from "@/utils/detectUser";
 
 const usersDSModule = usersDS();
 const userDSModule = userDS();
+const themeDS = applicationTheme();
 const router = useRouter();
+
+const theme = computed(() => {
+  return themeDS.theme;
+});
 
 onMounted(async () => {
   await usersDSModule.getUsers();
